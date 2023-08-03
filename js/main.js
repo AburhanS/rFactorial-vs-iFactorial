9898c425472e033f559f20de676d55417e8c25b2
Burhan <ahmetilov20@gmail.com>
2023-08-03 03:29:54 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/* const num = parseInt(prompt("What number do you want to find the factorial of?"));
+
+function factorial(num) {
+    if (num === 0 || num === 1) {
+        return 1;
+    } else if (num < 0) {
+        throw new Error("The factorial of a negative number is undefined.");
+    } else {
+        return num * factorial(num - 1);
+    }
+}
+
+console.log(factorial(num)); */
+
+
+
+/* function factorial(num) {
+    if (num < 0) {
+        throw new Error("The factorial of a negative number is undefined.");
+    }
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+const num = parseInt(prompt("What number do you want to find the factorial of?"));
+console.log(factorial(num)); */
